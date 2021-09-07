372dd3a0199cac8a7b0ab30dc67b6acdf91ae56d
Marius Ulbrich <marius.ulbrich@s-atiw.de>
2021-09-07 10:05:21 +0200

    Projektdateien hinzufügen.

---
+++ Escape Quiz/Cloze.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Escape_Quiz
+{
+    class Cloze
+    {
+
+        // Wird verwendet um Input zu normalisieren / auf leichte Fehler zu ueberpruefen.
+        public string NormalizeInput(string input)
+        {
+            input.ToLower();
+            string regexString = "[^A-Za-z0-9]";
+            Regex r = new Regex(regexString);
+            // Match target = ;
+           // MatchEvaluator evaluator = new MatchEvaluator(target);
+            // TODO Input normalisieren / auf leichte Fehler ueberpruefen.
+
+            return input;
+        }
+    }
+}
+
+
+/*
+ * Frame
+ * Create View Folder
+ * 
+ * 
+ * In Button
+ *  this.MainFrame.Navigate(new Quiz1View);
+ *  /
---
+++ Escape Quiz/MultipleChoice.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+// Datentyp zur Interaktion mit einer Multiple / Single Choice Frage
+public class MultipleChoice
+{
+	// enthaelt welche Antwort die richtige ist.
+	private bool[] optionsTrue;
+
+	// Antwortinhalt
+	private string[] options;
+
+	// Ob es Single oder Multiple Choice ist
+	private bool singleAnswer;
+
+
+
+	public MultipleChoice(bool singleAnswer, string[] answerStrings)
+	{
+		// Uebergabe des Parameters zur weiteren Verwendung
+		this.singleAnswer = singleAnswer;
+
+		// befuellen des Antworten Arrays mit einem Array aus der urspruenglichen Datenquelle
+		foreach (String answerstring in answerStrings)
+		{
+			options.Append(answerstring);
+			
+		}
+	}
+
+
+	// Ueberprueft ob die gegebene Antwort richtig ist.
+	public bool CheckAnswer(bool[] answers)
+	{
+		if (singleAnswer)
+		{
+			// Uebergibt die Richtigkeit der Antworten ins Objekt
+			foreach (bool answerbool in answers)
+			{
+				optionsTrue.Append(answerbool);
+			}
+
+			// Geht das Antwortenstring Arraz durch und schaut ob an der Stelle
+			// im AntwortenwahrheitenArray vermekt ist, ob die Antwort richtig ist.
+			for (int i = 0; i < options.Length; i++)
+			{
+
+				if (optionsTrue[i])
+				{
+					// TODO moegliche Farbaenderung fuer Richtigkeitsindikations
+					return true;
+				}
+				else {
+                }
+
+			}
+			// TODO nicht sicher ob das hier gut ist, da eigentlich ein Match gefunden werden sollte.
+			return false;
+		}
+		else {
+			// TODO Logik implementieren wie bei echter Multiple Choice zu agieren ist.
+			return false;
+		}
+
+		// TODO CheckAnswers() Logik wenn geklaert ist wie eine Antwort uebermittelt wird
+		
+	}
+}
