897d09b97c56d3211efd0a4e8314db8fba243b30
Marius Ulbrich <m.ulbrich@raynet.de>
2021-09-18 14:23:02 +0200

    Adding Property for Color Resources

--- Escape Quiz/Score.cs
+++ Escape Quiz/Score.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace Escape_Quiz
 {
     class Score
     {
         private static int finalScore;
+        public static Brush Right { get => new SolidColorBrush(Colors.Green); }
+        public static Brush Wrong { get => new SolidColorBrush(Colors.Red); }
 
         public Score()
         {
