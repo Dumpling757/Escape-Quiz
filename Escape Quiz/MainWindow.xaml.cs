f4fecf6136841d2cc5ed440a47ba53cc55962a5a
Katharina Rassloff <katharinarassloff@gmx.de>
2021-09-20 13:57:13 +0200

    Design fuq hoffentlich das letzte Mal

--- Escape Quiz/MainWindow.xaml.cs
+++ Escape Quiz/MainWindow.xaml.cs
@@ -31,27 +31,21 @@ namespace Escape_Quiz
             
         }
 
-        private void Button_GetHelp(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("IT Quiz - Get Help.\n\n" +
-                "Willkommen im Escape Room der FI194. Wie wir sehen, habt ihr den Einstieg erfolgreich geschafft, " +
-                "den PC repariert und den USB Stick gefunden. \n\n" +
-                "Eure Aufgabe ist es nun, dieses Quiz mit Themen rund um die Informationstechnologie zu bestehen, " +
-                "um einen benötigten Code für die nächste Station zu erhalten. \n\n" +
-                "HINWEIS\n" +
-                "Sei aufmerksam und notiere dir wichtige Informationen, denn zurückgehen kannst du nicht! \n\n" +
-                "Und nun: VIEL SPAß!");
-        }
+        
 
         private void Button_QuizStart(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("STOP\n\n" +
-                "Du kannst nicht zurückgehen. Gehe erst zur nächsten Frage weiter, wenn du die Aufgabe erledigt hast.\n\n" +
-                "HINWEIS\n" +
-                "Am Ende jeder Frage bekommst du Informationen, die dir nach dem Quiz sehr hilfreich sein könnten. Notiere sie!!");
+            MessageBox.Show("IT Quiz - Get Help.\n\n" +
+                 "Willkommen im Escape Room der FI194. Wie wir sehen, habt ihr den Einstieg erfolgreich geschafft, " +
+                 "den PC repariert und den USB Stick gefunden. \n\n" +
+                 "Eure Aufgabe ist es nun, dieses Quiz mit Themen rund um die Informationstechnologie zu bestehen, " +
+                 "um einen benötigten Code für die nächste Station zu erhalten. \n\n" +
+                 "HINWEIS\n" +
+                 "Sei aufmerksam, denn zurückgehen kannst du nicht! \n\n" +
+                 "Und nun: VIEL SPAß!");
 
-             this.MainFrame.Navigate(new Views.Wahrheitswerttabelle(this.MainFrame));
-           // this.MainFrame.Navigate(new Views.PictureDrag(this.MainFrame));
+            this.MainFrame.Navigate(new Views.Wahrheitswerttabelle(this.MainFrame));
+           
         }
 
         private void MainFrame_Navigated(object sender, NavigationEventArgs e)
